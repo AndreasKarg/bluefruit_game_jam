e0bc6a00470f8b0cd3501bda98ed06dd945b92a6
Andreas Karg <andreas.karg@bluefruit.co.uk>
2021-05-30 10:12:14 +0100

    Throw down some theming ideas

--- experiments/src/todo.rs
+++ experiments/src/todo.rs
@@ -9,3 +9,15 @@
 // TODO: Special launch/return mechanics per unit type
 // TODO: In-base movement
 // TODO: Pause and stopping at game over
+/// Theme
+/// -----
+///
+/// Ze evil forreners from Grapenia are invading from ze South!
+///
+/// Zey are after our preshis plum orchards and vant to make zem into vineyards for zeir terribel
+/// rotten juice drug drink. God only knows vhy zey like it so much.
+/// Ze Kaiser has ordered YOU to run ze main defense operation agenst ze Ennemy. Zey are stronk.
+/// Hold zem off for as long as you can and ve vill propose you for ze Eiserne Pflaume medal!
+///
+/// [For ze Kaiser! For PLUMANNY!!]
+struct Ideas {}
